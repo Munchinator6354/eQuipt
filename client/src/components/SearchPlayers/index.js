5e5fd55bf53ac7152fa158c74fd04e525dd82ed9
Ryan Isaacson <munchinator6354@gmail.com>
2020-02-01 13:30:25 -0800

    added Create, Search, Trade, & Give components, each with index.js importing React

---
+++ client/src/components/SearchPlayers/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div>
+            <h1>Search Players</h1>
+        </div>
+    )
+}
---
+++ client/src/components/Trade/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div>
+            <h1>Trade Items</h1>
+        </div>
+    )
+}
