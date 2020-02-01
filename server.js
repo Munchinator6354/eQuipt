387839e957d858633aed412b6460b3feae7a8e17
jsills3110 <jsills3110@gmail.com>
2020-02-01 14:10:35 -0800

    Added skeleton routes to the apiroutes.js file. Installed passport-local. Modified server.js file to retrieve routes from the routes folder.

--- server.js
+++ server.js
@@ -6,18 +6,23 @@ const app = express();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-// Define API routes here
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// ================================================================================
+// API ROUTER
+// ================================================================================
+
+require("./routes/apiRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
