e0326610feb15b908d0c20fe2c3699d53eef399f
Noelle Everage <neverage84@yahoo.com>
2020-02-03 21:27:40 -0800

    Original mongoose schemas for inventory and user

---
+++ models/User.js
@@ -0,0 +1,47 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+var UserSchema = new Schema({
+ 
+  playername: {
+    type: String,
+    unique: true
+  },
+  username: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    unique: true
+  },
+  charactername: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  // `notes` is an array that stores ObjectIds
+  // The ref property links these ObjectIds to the Note model
+  // This allows us to populate the User with any associated Notes
+  items: [
+    {
+      // Store ObjectIds in the array
+      type: Schema.Types.ObjectId,
+      // The ObjectIds will refer to the ids in the Note model
+      ref: "Inventory"
+    }
+  ]
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var User = mongoose.model("User", UserSchema);
+
+// Export the User model
+module.exports = User;
