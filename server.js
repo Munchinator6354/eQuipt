75da3a67bad452f7f1cdfa186322a49139c88e29
jsills3110 <jsills3110@gmail.com>
2020-02-04 21:37:54 -0800

    Upgraded concurrently in package.json. Corrected APIs so that JSON was json for all app function calls. Set API routes above the wildcard route in server.js.

--- server.js
+++ server.js
@@ -26,17 +26,18 @@ db.Inventory.insertMany(inventoryseed)
   .catch(function(err) {
     console.log(err.message);
   });
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 // ================================================================================
 // API ROUTER
 // ================================================================================
 
-// require("./routes/apiRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
